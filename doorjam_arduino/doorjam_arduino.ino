4328dc1ba808394e6df4eb02761e4bb61753360d
jerkey <jerkey@halo.nu>
2014-07-05 01:07:28 -0700

    more comments

--- doorjam_arduino/doorjam_arduino.ino
+++ doorjam_arduino/doorjam_arduino.ino
@@ -27,9 +27,9 @@
 #define CHB_BRK 8
 #define CHB_SENSE A1
 
-#define OPEN_TIME 14000
+#define OPEN_TIME 14000 // milliseconds how long to run the motor
 #define CLOSE_TIME 14000
-#define OPEN_CURRENTMAX 140
+#define OPEN_CURRENTMAX 140 // analogRead() value, not amps or anything
 #define CLOSE_CURRENTMAX 140 // change this to 70 to be able to trip it by hand
 #define MOTOR_START_TIME 150 // milliseconds before we start looking at current draw
 
