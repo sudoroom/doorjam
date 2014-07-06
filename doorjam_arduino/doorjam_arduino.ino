e6dd51d1ef62d56768d900b749d70e1a22c72dd6
Andrew Lowe <andrew@lostways.com>
2014-07-06 02:59:31 -0700

    removed led strip testing

--- doorjam_arduino/doorjam_arduino.ino
+++ doorjam_arduino/doorjam_arduino.ino
@@ -41,7 +41,7 @@
 
 #define AVG_CYCLES 50.0 // how many times to read analogRead and average reading
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(6, LED_DATA, NEO_GRB + NEO_KHZ800);
+#Adafruit_NeoPixel strip = Adafruit_NeoPixel(6, LED_DATA, NEO_GRB + NEO_KHZ800);
 
 
 
@@ -53,43 +53,10 @@ void sadTone() {
   tone (SPEAKER, SADTONE, SADTIME);
 }
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-        }
-        strip.show();
-       
-        delay(wait);
-       
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, 0);        //turn every third pixel off
-        }
-    }
-  }
-}
-
 
 void doorOpen() {
   Serial.println("opening door!");
-  happyTone(); // tone is not blocking
-  theaterChaseRainbow(50);  
+  happyTone(); // tone is not blocking  
   digitalWrite(CHB_DIR,LOW);
   digitalWrite(CHB_PWM,HIGH); // turn motor on
   unsigned long now = millis();
