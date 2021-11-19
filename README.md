ec6dd43c654d450cb76fc457ad497091bd87d955
Yardena Cohen <yardenack@gmail.com>
2021-11-18 18:21:24 -0800

    prep for merge

--- README.md
+++ README.md
@@ -16,6 +16,32 @@ If the card is not recognized then its hash is logged in the failed_attempts fil
 
 Swipe a new card and run the grant_access_to_last_failed_attempt.js script to grant people access.
 
+*Warning: If the last failed swipe was improperly registered, then you could potentially be granting access to one of several common error codes, rendering the system insecure. See the bottom of this README for common magstripe error code hashes to blacklist.*
+
+People seeking access can also swipe their own cards (at least 3x) and fill out the form at https://omnicommons.org/keys/
+
+Recommended cards are Safeway, Peets, and other store membership cards. Hotel keycards do not work. Recommend that folks not use cards with identifying or otherwise valuable information such as credit cards.
+
+## Step-by-step:
+* Ensure you're on the local SSID peoplesopen.net or otherwise logged into room.sudoroom.org
+* You must be on a mac or linux machine with avahi-daemon installed
+```
+ssh root@omnidoor.local 
+
+rwroot
+
+tail -f /var_rw/failed_attempts
+```
+* Swipe card three times
+* Ensure last 3 failed attempts match each other
+```
+./grant_access_to_last_attempt.js <name>
+```
+* Check that card works!
+```
+roroot
+```
+
 # Autostart on boot
 
 ## With forever (autostart on fail)
@@ -46,11 +72,16 @@ ssh root@192.168.1.2 "cd /root/doorjam; ./grant_access_to_last_attempt.js <name 
 
 You can also use the sudo_grant_access.sh script, but you should edit it to suit your environment.
 
+change /etc/bash.bashrc to remind you when the filesystem is not read-only:
+# PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+PS1='${debian_chroot:+($debian_chroot)}\u@\h`if grep "ec2c7555affb / ext4 rw" /proc/mounts > /dev/null; then echo \ FILESYSTEM IS WRITABLE, RUN roroot TO FIX; fi`:\w\$ '
+
 # License and copyright
 
 GPLv3+
 
 Copyright 2014 Marc Juul
+
 blacklist the following hashes (magstripe error codes):
 b8548d912086cc19f0f4633a4307af3c35b6ea81
 dd51c7f8b6b1c7b478217b10cb228a0d1d5fb310
@@ -58,3 +89,7 @@ dd51c7f8b6b1c7b478217b10cb228a0d1d5fb310
 05475cb790516f8bdd63504f24c295ddea6f086a (i think)
 1177c84cedb98aca496cb2997d84f654a902f1dc
 2d71b8483fb11a1e52849c3ce54c07f43b640852
+684f2ce81f430e49406911da84c44cc3a9b65230
+7d7b38dbdd71860c9c4502505a0cb5842bfa9fe6
+4adc1dbd9253e3ec41bc037d2f5fd393c790ece5
+619042c03c64bb9049b43301ab9b9dd52ec0b161
--- grant_access_to_last_attempt.js
+++ grant_access_to_last_attempt.js
@@ -5,37 +5,87 @@
 */
 
 var fs = require('fs');
+var spawn = require('child_process').spawn;
+var split = require('split2');
+var through = require('through2');
+var strftime = require('strftime');
 
-if(!fs.existsSync('failed_attempts')) {
-    console.log("It appears that there have been no failed attempts (the failed_attempts file doesn't exist)");
+if(!fs.existsSync('/var_rw/failed_attempts')) {
+    console.log("It appears that there have been no failed attempts (the /var_rw/failed_attempts file doesn't exist)");
     process.exit(1);
 }
 
 if(process.argv.length < 3) {
-    console.log("Usage: "+process.argv[1]+" <name and contact info for new user>")
+    console.log("Usage: "+process.argv[1]+" <name of new user>")
     process.exit(1);
 }
 
-var attempts = fs.readFileSync('failed_attempts', {encoding: 'utf8'}).split("\n");
-
+var attempts = fs.readFileSync('/var_rw/failed_attempts', {encoding: 'utf8'}).split("\n");
 var i, lastAttempt;
 for(i=attempts.length-1; i >= 0; i--) {
     lastAttempt = attempts[i];
     if(lastAttempt.replace(/^\s+$/g, '').length > 4) {
         lastAttempt = JSON.parse(lastAttempt);
+        if (/^less than/.test(lastAttempt.code)) {
+            console.error('last failed attempt was a bad read');
+            process.exit(1);
+        }
         break;
     }
-} 
-
-var comment = '# ' + process.argv.slice(2).join(' ') + " | added on " + new Date() + "\n";
-
-var entry = comment + lastAttempt.code + "\n";
-
-try {
-    fs.appendFileSync('access_control_list', entry);
-} catch(e) {
-    console.log("Error: Hm. Are you sure you have permission to write to the access_control_list file?")
-    process.exit(1);
 }
 
-console.log("Added: \n" + entry);
+askPrompts([
+    { key: 'announce', msg: 'How should the IRC bot announce this user?' },
+    { key: 'contactInfo', msg: 'What is their email and/or phone number?' },
+    { key: 'collective', msg: 'Which collective are they a member of?' },
+    { key: 'addedBy', msg: 'Who are you, the person granting access?' }
+], addUser);
+
+function askPrompts (prompts, cb) {
+    var current = prompts.shift();
+    var answers = {};
+    process.stdout.write(current.msg + ' ');
+    process.stdin.pipe(split()).pipe(through(function (buf, enc, next) {
+        var line = buf.toString();
+        if (!/\S/.test(line)) {
+            process.stdout.write(current.msg + ' ');
+            return next();
+        }
+        answers[current.key] = line;
+        if (prompts.length > 0) {
+            current = prompts.shift();
+            process.stdout.write(current.msg + ' ');
+            next();
+        } else cb(answers);
+    }));
+}
+
+function addUser (answers) {
+    var comment = '# ' + process.argv.slice(2).join(' ') + " | added on " + new Date() + process.env.SSH_CLIENT + process.env.TERM + " user: " + process.env.USER + "\n";
+    var name = process.argv.slice(2).join(' ')
+    var comment = '#ANNOUNCE ' + JSON.stringify(answers.announce) + ' ' + name
+        + ' ' + answers.contactInfo
+        + ' ' + answers.collective
+        + ' | added by ' + answers.addedBy
+        + ' (' + process.env.USER
+        + ') on ' + strftime('%FT%T')
+        + '\n';
+
+    // #ANNOUNCE "gumby" ben bgdaniels@gmail.com / commons wg | added by jake Wed Feb 10 22:29:19 PST 2016
+
+    var entry = comment + lastAttempt.code + "\n";
+
+    try {
+        spawn('/bin/mount',[ '-o','remount,rw','/' ])
+          .stdout.on('end', function(){
+            fs.appendFileSync(__dirname + '/access_control_list', entry);
+            spawn('/usr/local/bin/roroot');
+          })
+    } catch(e) {
+        console.log("Error: Hm. Are you sure you have permission to write to the access_control_list file?")
+        process.exit(1);
+    }
+
+    console.log("Added: \n" + entry);
+    process.stdin.destroy();
+}
